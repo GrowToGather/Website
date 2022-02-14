d32f7deec8e2ed5b16a6c01296e6e4b89499c50c
fabian0141 <fabian0141@gmx.de>
2022-02-14 22:20:43 +0100

    4 files modified

---
+++ src/Api.js
@@ -0,0 +1,4 @@
+import SwaggerUI from 'swagger-ui-react';
+import 'swagger-ui-react/swagger-ui.css';
+
+export default App = () => <SwaggerUI url="../BackendAPI.yaml" />;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from "react-dom";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import './index.css';
