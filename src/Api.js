923ea67d297ff50b50fbb79f8d917ba6e2bb84f5
fabian0141 <fabian0141@gmx.de>
2022-02-14 22:33:47 +0100

    2 files modified, 1 deleted

--- src/Api.js
+++ src/Api.js
@@ -1,4 +1,11 @@
+import React from 'react';
 import SwaggerUI from 'swagger-ui-react';
 import 'swagger-ui-react/swagger-ui.css';
 
-export default App = () => <SwaggerUI url="../BackendAPI.yaml" />;
+function Api() {
+  return (
+    <SwaggerUI url="https://raw.githubusercontent.com/GrowToGather/Website/main/BackendAPI.yaml" />
+  );
+}
+
+export default Api;
