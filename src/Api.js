fd6050555daa0d715d92c8393423bed62df41654
Fabian Wigger <fabian0141@gmx.de>
2022-02-15 14:12:48 +0100

    Fix buffer issue

--- src/Api.js
+++ src/Api.js
@@ -1,6 +1,9 @@
 import React from 'react';
+import {Buffer} from 'buffer';
 import SwaggerUI from 'swagger-ui-react';
 import 'swagger-ui-react/swagger-ui.css';
+
+window.Buffer = Buffer
 
 function Api() {
   return (
