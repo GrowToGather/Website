7c744086570ccdde8a8e07e168c49e3698725bf5
andreas <freitandreas@gmail.com>
2022-05-11 03:39:59 +0200

    basics

--- src/Socials/Matching/online-friendshipping_matching.go
+++ src/Socials/Matching/online-friendshipping_matching.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+	"sort"
 	"strconv"
 	"time"
 )
@@ -22,17 +24,94 @@ func getAge(birthday string) int {
 }
 
 type Participant struct {
-	name string
-	age  int
+	id      int
+	name    string
+	age     int
+	matched []int
 }
 
 type Match struct {
+	parts   []Participant
+	matches []Room
+	score   int
+}
+
+type Room struct {
+	part1 Participant
+	part2 Participant
+}
+
+func recent(part int, data []int) int {
+	for k, v := range data {
+		if part == v {
+			return k
+		}
+	}
+	return len(data)
+}
+
+func indexOf(part int, data []Participant) int {
+	for k, v := range data {
+		if part == v.id {
+			return k
+		}
+	}
+	return len(data)
+}
+
+func generateMatches(parts []Participant) []Room {
+	rand.Shuffle(len(parts), func(i, j int) {
+		parts[i], parts[j] = parts[j], parts[i]
+	})
+	var matches []Room
+	for i := 0; i < int(len(parts)/2); i++ {
+		matches = append(matches, Room{parts[i], parts[i+1]})
+	}
+	return matches
+}
+
+func (match Match) evaluateMatches() int {
+	score := 0
+	for i := 0; i < len(match.matches); i++ {
+		score -= recent(match.matches[i].part1.id, match.matches[i].part2.matched) +
+			recent(match.matches[i].part2.id, match.matches[i].part1.matched)
+	}
+	return score
 }
 
 func main() {
-	part := Participant{"Andreas", getAge("1999-07-17")}
 
-	output := strconv.Itoa(part.age)
-	fmt.Printf(output)
+	var parts []Participant
+	for i := 0; i < 10; i++ {
+		parts = append(parts,
+			Participant{i, "P" + strconv.Itoa(i), rand.Intn(13) + 18, []int{}})
+	}
+	for round := 1; round < 100; round++ {
+		var matches []Match
+		for i := 0; i < 100; i++ {
+			matches = append(matches, Match{parts, generateMatches(parts), 0})
+			matches[i].evaluateMatches()
+		}
+
+		for iter := 0; iter < 100; iter++ {
+			sort.Slice(matches, func(i, j int) bool {
+				return matches[i].score < matches[j].score
+			})
+			for i := 10; i < len(matches); i++ {
+				matches[i] = Match{parts, generateMatches(parts), 0}
+				matches[i].score = matches[i].evaluateMatches()
+			}
+		}
+		fmt.Print(strconv.Itoa(matches[0].score) + "\n")
+		for i := 0; i < len(matches[0].matches); i++ {
+			parts[indexOf(matches[0].matches[i].part1.id, parts)].matched =
+				append(parts[indexOf(matches[0].matches[i].part1.id, parts)].matched, matches[0].matches[i].part2.id)
+			parts[indexOf(matches[0].matches[i].part2.id, parts)].matched =
+				append(parts[indexOf(matches[0].matches[i].part2.id, parts)].matched, matches[0].matches[i].part1.id)
+		}
+		parts = matches[0].parts
+		fmt.Print(matches[0].parts[0].matched)
+		fmt.Print("\n")
+	}
 
 }
